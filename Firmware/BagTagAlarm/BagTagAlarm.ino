ba765a01728fd460e4db15b39abc1a074f777ce5
Kkuwata_imac <kevinkuwata@icloud.com>
2018-04-11 20:31:52 -0600

    i have a sensitivity of 100 points change to see if the accl would work. it was working at 5 but then got super sensitive. so i moved it to 100. i'm using absolute value, and just want to see the alarm trigger. i then need to do a sensitivity test like if it has movoment for over 750 mS then make the alamr

--- Firmware/BagTagAlarm/BagTagAlarm.ino
+++ Firmware/BagTagAlarm/BagTagAlarm.ino
@@ -53,6 +53,24 @@ SOFTWARE.
 #define BIT6 0b01000000
 #define BIT7 0b10000000
 
+#define ACCELEROMETER_AVG_COUNT 		5 //arbitrary averaging
+#define ACCELEROMETER_SENSITIVY_X		100 // again arbitrarily chosen
+#define ACCELEROMETER_SENSITIVY_Y		5
+#define	ACCELEROMETER_SENSITIVY_Z		5
+
+
+
+/* ====================================================================== *//* ====================================================================== */
+/* ====================================================================== *//* ====================================================================== */
+///All of my defines for Testing	
+#define TEST_ACCL			//turns on an led if change in motion
+//#define TEST_ALARM		//tests the alarm
+
+//#define TEST_SOLIDLED 		//I THINK WE HAVE A BUNCH OF RESETS.
+
+/* ====================================================================== */
+/* ====================================================================== *//* ====================================================================== */
+
 
 void setup(){
 	pinMode(LED0_D2, OUTPUT);
@@ -64,14 +82,116 @@ void setup(){
 	pinMode(ACCELEROMETER_X, INPUT);
 	pinMode(ACCELEROMETER_Y, INPUT);
 	pinMode(ACCELEROMETER_Z, INPUT);
+	
+	digitalWrite(ACCELEROMETER_PWR_DOWN, LOW);	//NORMAL MODE, NOT SLEEP
+	digitalWrite(ACCELEROMETER_SELF_TEST, LOW); //NORMAL MODE
 
+	//blink the led so we know code is running
+	for(int i=0; i < 10; i++){
+		digitalWrite(LED0_D2, HIGH);
+		delay(100);
+		digitalWrite(LED0_D2, LOW);
+		delay(100);
+	}
+	
 	
 	digitalWrite(LED0_D2, LOW);
 	digitalWrite(ALARM, LOW);
 	
+	#ifdef TEST_SOLIDLED
+	digitalWrite(LED0_D2, HIGH);
+	while(1);
+	#endif
+	
+	
 }
 
+#ifdef TEST_SOLIDLED
+void loop(){
+	while(1);
+}
+#endif
 
+
+/*
+	@name: void loop()
+	@brief: tests the accelerometer by averaging the x, y, z values.	
+		if significant change light the on board led on pin 2
+		update the average every 20 cycles, arbitrarily chosen. 
+	
+*/
+#ifdef TEST_ACCL
+
+unsigned int x_measure = 0;
+unsigned int y_measure = 0;
+unsigned int z_measure = 0;
+unsigned int old_X_average = 0;
+unsigned int old_Y_average = 0;
+unsigned int old_Z_average = 0;
+
+//bool SOUND_THE_ALARM = false; 
+
+void loop(){
+	//reset
+	x_measure = 0;
+	y_measure = 0;
+	z_measure = 0;
+	
+	//collect for average
+	for(int i=0; i <ACCELEROMETER_AVG_COUNT; i++){
+	x_measure += analogRead(ACCELEROMETER_X);
+	y_measure += analogRead(ACCELEROMETER_Y);
+	z_measure += analogRead(ACCELEROMETER_Z);
+	}
+	
+	//average
+	x_measure /= ACCELEROMETER_AVG_COUNT;
+	y_measure /= ACCELEROMETER_AVG_COUNT;
+	z_measure /= ACCELEROMETER_AVG_COUNT;
+
+	//calculate change
+	byte x_change = abs(x_measure - old_X_average);
+	byte y_change = abs(y_measure - old_Y_average);
+	byte z_change = abs(z_measure - old_Z_average);
+	
+	//if bigger than threshold
+	//if( (x_change> ACCELEROMETER_SENSITIVY_X ) || (y_change > ACCELEROMETER_SENSITIVY_Y) || (z_change > ACCELEROMETER_SENSITIVY_Z)){
+	if( x_change > ACCELEROMETER_SENSITIVY_X ){
+		digitalWrite(LED0_D2, HIGH);
+		delay(200);
+		digitalWrite(LED0_D2, LOW);
+		delay(1000);
+		
+		//SOUND_THE_ALARM = true;
+	}
+	
+	/*
+	if(SOUND_THE_ALARM == true){
+		soundAlarm(2); //sound alarm for 2 seconds
+		SOUND_THE_ALARM = false;
+	}
+	*/
+	
+	
+	//update values;
+	old_X_average = x_measure;
+	old_Y_average = y_measure;
+	old_Z_average = z_measure;
+	
+}
+#endif
+
+/*
+	@name: void loop()
+	@brief: will test the alarm by just port mainpulation. assuming set up was done correctly. 
+	@input: none
+	@output: 4khz 50% square wave 3.3v
+	
+	@bug: 11khz chopping frequency plus the 4khz frequency from the square wave. 50% duty cycle spot on.
+	
+	something about how the loop works. 
+*/
+#ifdef TEST_ALARM
 void loop(){
 	while(1){
 	PORTD |=BIT5; // turn on
@@ -80,6 +200,9 @@ void loop(){
 	delayMicroseconds(125);//	heartBeat();
 	}
 }
+
+#endif
+
 /*
 	@name: soundalarm
 	@brief: will sound the piezeo buzzer with 50% duty cycle square wave with 4Khz. This produces the loudest alarm. 
@@ -89,10 +212,21 @@ void loop(){
 	@input: duration = a byte to describe how long to sound alarm in seconds. 
 */
 void soundAlarm(byte duration){
+	
+	PORTD |= BIT2; // on led
+
+	long startTime = millis();
+	long elapsedTime = 0;
+	while((elapsedTime - startTime) > (duration * 1000)){
 	PORTD |=BIT5; // turn on
 	delayMicroseconds(125);
 	PORTD ^= ~(BIT5); //turn off
 	delayMicroseconds(125);
+	elapsedTime = millis(); //update how much time has gone by. 
+	}
+	
+	PORTD ^= ~BIT2; //off
+	delay(1000); //so we know its really off. 
 }	
 
 void heartBeat(){
